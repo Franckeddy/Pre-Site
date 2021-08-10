ade438f40782c26373fc65ca89edc2f901db6f47
garcon franck <franck.garcon@gmail.com>
2021-08-10 14:50:02 +0200

    cs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    api: "http://localhost:8888/e-commerce/backend/api",
+    api: "http://localhost:8888/e-commerce/backend/api/",
     api_key: "adsffsdfds6b-6727-46f4-8bee-2c6ce6293e41",
 };
 
